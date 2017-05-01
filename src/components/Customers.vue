e14e6a4a910caa25ffed89acb03726521be34d7e
zelazna <constantin.guidon@gmail.com>
2017-05-01 15:14:47 +0200

    first commit

---
+++ src/components/Customers.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container">
+    <h1>Customers</h1>
+    <div class="center">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Nationalite</th>
+            <th>Mail</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="customer in customers">
+            <td>{{customer.firstName}}</td>
+            <td>{{customer.lastName}}</td>
+            <td>{{customer.nationalite}}</td>
+            <td>{{customer.mail}}</td>
+            <td>del update</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customers',
+  data () {
+    return {
+      customers: '',
+      instance: this.$http.create({
+        baseURL: 'http://localhost:3000',
+        // auth: {
+        //   username: 'root',
+        //   password: 'root'
+        // },
+        headers: { 'x-auth-token': localStorage.getItem('token') }
+      })
+    }
+  },
+  mounted: function () {
+    this.instance
+      .get('/customers')
+      .then(data => {
+        this.customers = data.data.customers
+      })
+      .catch(err => console.log(err))
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+th {
+  text-align: center
+}
+
+table {
+  width: 75%
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+}
+</style>
